bdb37c683f240274c89571e776adf38261bf0cb8
S203abel <gebruabel7@gmail.com>
2023-10-20 15:03:38 -0700

    update

--- main.h
+++ main.h
@@ -78,39 +78,39 @@ unsigned int convert_o(va_list args, buffer_t *output,
 unsigned int convert_x(va_list args, buffer_t *output,
 		unsigned char flags, int wid, int prec, unsigned char len);
 unsigned int convert_X(va_list args, buffer_t *output,
-		unsigned char flags, int wid, int prec, unsigned char len);
-unsigned int convert_S(va_list args, buffer_t *output,
-		unsigned char flags, int wid, int prec, unsigned char len);
-unsigned int convert_p(va_list args, buffer_t *output,
-		unsigned char flags, int wid, int prec, unsigned char len);
-unsigned int convert_r(va_list args, buffer_t *output,
-		unsigned char flags, int wid, int prec, unsigned char len);
-unsigned int convert_R(va_list args, buffer_t *output,
-		unsigned char flags, int wid, int prec, unsigned char len);
+		unsigned char flags, int width, int precision, unsigned char len);
+unsigned int convert_S(va_list arg, buf_er *output,
+		unsigned char flags, int width, int precision, unsigned char len);
+unsigned int convert_p(va_list arg, buf_er *output,
+		unsigned char flags, int width, int precision, unsigned char len);
+unsigned int convert_r(va_list arg, buf_er *output,
+		unsigned char flags, int width, int precision, unsigned char len);
+unsigned int convert_R(va_list arg, buf_er *output,
+		unsigned char flags, int width, int precision, unsigned char len);
 
 /* Handlers */
 unsigned char handle_flags(const char *flags, char *index);
 unsigned char handle_length(const char *modifier, char *index);
-int handle_width(va_list args, const char *modifier, char *index);
-int handle_precision(va_list args, const char *modifier, char *index);
-unsigned int (*handle_specifiers(const char *specifier))(va_list, buffer_t *,
+int handle_width(va_list arg, const char *modifier, char *index);
+int handle_precision(va_list arg, const char *modifier, char *index);
+unsigned int (*handle_specifiers(const char *specifier))(va_list, buf_er *,
 		unsigned char, int, int, unsigned char);
 
 /* Modifiers */
-unsigned int print_width(buffer_t *output, unsigned int printed,
-		unsigned char flags, int wid);
-unsigned int print_string_width(buffer_t *output,
-		unsigned char flags, int wid, int prec, int size);
-unsigned int print_neg_width(buffer_t *output, unsigned int printed,
-		unsigned char flags, int wid);
+unsigned int print_width(buf_er *output, unsigned int printed,
+		unsigned char flags, int width);
+unsigned int print_string_width(buf_er *output,
+		unsigned char flags, int width, int precision, int size);
+unsigned int print_neg_width(buf_er *output, unsigned int printed,
+		unsigned char flags, int width);
 
 /* Helper Functions */
-buffer_t *init_buffer(void);
-void free_buffer(buffer_t *output);
-unsigned int _memcpy(buffer_t *output, const char *src, unsigned int n);
-unsigned int conv_sb(buffer_t *output, long int num, char *base,
-		unsigned char flags, int wid, int prec);
-unsigned int conv_ub(buffer_t *output, unsigned long int num, char *base,
-		unsigned char flags, int wid, int prec);
+buf_fer *init_buffer(void);
+void free_buffer(buf_er *output);
+unsigned int _memcpy(buf_er *output, const char *src, unsigned int n);
+unsigned int conv_sb(buf_er *output, long int num, char *base,
+		unsigned char flags, int width, int precision);
+unsigned int conv_ub(buf_er *output, unsigned long int num, char *base,
+		unsigned char flags, int width, int precision);
 
 #endif /* MAIN_H */
